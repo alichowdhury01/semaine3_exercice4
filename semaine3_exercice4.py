d4e2c5bcc7e2f0a5260f3b8fb898f9ff3b35afee
alichowdhury01 <39740365+alichowdhury01@users.noreply.github.com>
2022-03-08 09:46:17 -0500

    Update semaine3_exercice4.py

--- semaine3_exercice4.py
+++ semaine3_exercice4.py
@@ -1,11 +1,16 @@
 #Créez un programme demandant à un utilisateur d'entrer sa date de fête et retournez-lui sa saison de naissance 
 #s'il est né dans l'hémisphère Nord. (Vous pouvez assumer que les équinoxes et solstices ont lieu le 21 du mois.)
+#primtemps hémisphère nord 21 mars 21 juin
+#été hémisphère nord 22 juin 22 sept
+#automne hémisphère 22 sept 21 dec
+#hiver hémisphère nord 22 dec 20 mars
 
 def saison_de_naissance():
-    
+    if mois 
 
 jour = int(input("Quel est le jour de votre anniversaire:"))
 mois = int(input("Quel est le mois de votre anniversaire:"))
+hemisphere = str(input("Êtes-vous dans l'hémisphère nord ou sud:"))
 
 saison = ["Automne", "Hiver", "Printemps", "Été"]
 
